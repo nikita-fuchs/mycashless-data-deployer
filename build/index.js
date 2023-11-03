b08fb4f86ad8bdb4b5f01e56b1a3e16302b03fb4
Nikita Fuchs <q.fuchs@gmail.com>
2023-11-03 13:38:40 +0100

    genereate and gitignore the keys

--- build/index.js
+++ build/index.js
@@ -1,1 +1,6 @@
-console.log("test");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { AeSdk, MemoryAccount, Node, CompilerHttp, AE_AMOUNT_FORMATS, generateKeyPair } = require('@aeternity/aepp-sdk');
+const keypair = generateKeyPair();
+console.log(`Secret key: ${keypair.secretKey}`);
+console.log(`Public key: ${keypair.publicKey}`);
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("test")
+export {}; //handle typescript bs https://stackoverflow.com/questions/35758584/cannot-redeclare-block-scoped-variable
+
+const {
+    AeSdk,
+    MemoryAccount,
+    Node,
+    CompilerHttp,
+    AE_AMOUNT_FORMATS,
+    generateKeyPair
+  } = require('@aeternity/aepp-sdk')
+const { secretKey, publicKey } = require('./keys.json') 
+  
